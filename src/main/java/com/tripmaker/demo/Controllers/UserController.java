f2423d10907e0c7f2ec2a0597af684980a926a08
jsierzega <jsierzega@student.wat.edu.pl>
2018-06-18 16:31:33 +0200

    Usunięcie błędu z restem usera

--- src/main/java/com/tripmaker/demo/Controllers/UserController.java
+++ src/main/java/com/tripmaker/demo/Controllers/UserController.java
@@ -6,15 +6,13 @@ import com.tripmaker.demo.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @CrossOrigin(origins = "*")
+@RestController
 @RequestMapping("api/user")
 public class UserController {
 
